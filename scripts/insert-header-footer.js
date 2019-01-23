9b40fc595114b4ee0ce983dd44b6dc6a7c49b262
Jack Buehner <jackebuehner@gmail.com>
2019-01-23 18:08:33 -0500

    fix sidenav not opening due to broken script

--- scripts/insert-header-footer.js
+++ scripts/insert-header-footer.js
@@ -17,11 +17,13 @@ $(document).ready(function(){
     $(this).children('.dropdown-content').toggle();
  	});
 
-//underline current page in navigation
-$(function(){
-	$('a').each(function(){
-    if ($(this).prop('href') == window.location.href) {
-      $(this).addClass('current-page'); $(this).parents('li.dropdown').addClass('current-dropdown');
-    }
-  });
+	//underline current page in navigation
+	$(function(){
+		$('a').each(function(){
+	    if ($(this).prop('href') == window.location.href) {
+	      $(this).addClass('current-page'); $(this).parents('li.dropdown').addClass('current-dropdown');
+	    }
+	  });
+	});
+
 });
