f32ec54a376a1802c1b8f3bc53372dd38d365007
Jack Buehner <jackebuehner@gmail.com>
2019-02-07 21:52:17 -0500

    fix scout weekend sign up

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'troop370_'
-var VERSION = 'version_54' // update the version every time the website is updated (or the content will not update on PWAs)
+var VERSION = 'version_55' // update the version every time the website is updated (or the content will not update on PWAs)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
   './index.html?utm_source=homescreen',
