1f2fb11fc397193522826d12e63926f9b5f2463d
Jack Buehner <jackebuehner@gmail.com>
2019-04-22 22:35:11 -0400

    add contact pages

--- js/main(source).js
+++ js/main(source).js
@@ -10,3 +10,17 @@ iconButtonRipple.unbounded = true;
 const ripple = [].map.call(document.querySelectorAll('li>a'), function(el) {
   return new MDCRipple(el);
 });
+
+
+import {MDCTextField} from '@material/textfield';
+
+const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
+  return new MDCTextField(el);
+});
+
+
+import {MDCNotchedOutline} from '@material/notched-outline';
+
+const notchedOutline = [].map.call(document.querySelectorAll('.mdc-notched-outline'), function(el) {
+  return new MDCNotchedOutline(el);
+});
