fea42ab4a8d5075899864bfca4f91ec735bde95c
Jack Buehner <jackebuehner@gmail.com>
2019-10-21 22:54:20 -0400

    add material form elements

--- js/main(source).js
+++ js/main(source).js
@@ -15,13 +15,26 @@ const ripple = [].map.call(document.querySelectorAll('.mdc-ripple-surface'), fun
   return new MDCRipple(el);
 });
 
-
+import {MDCFormField} from '@material/form-field';
 import {MDCTextField} from '@material/textfield';
+import {MDCCheckbox} from '@material/checkbox';
+import {MDCSelect} from '@material/select';
 
+const formField = [].map.call(document.querySelectorAll('.mdc-form-field'), function(el) {
+  return new MDCFormField(el);
+});
 const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
   return new MDCTextField(el);
 });
-
+const checkbox = [].map.call(document.querySelectorAll('.mdc-checkbox'), function(el) {
+  return new MDCCheckbox(el);
+});
+const select = [].map.call(document.querySelectorAll('.mdc-select'), function(el) {
+  return new MDCSelect(el);
+});
+select.listen('MDCSelect:change', () => {
+  alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+});
 
 import {MDCNotchedOutline} from '@material/notched-outline';
 
