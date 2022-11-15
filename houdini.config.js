b88ac3921a7c4d00fd569744e2424a466492c3ee
Jack Buehner <jack.buehner@troop370atlanta.org>
2022-11-15 18:39:27 -0500

    chore: apply prettier code style

--- houdini.config.js
+++ houdini.config.js
@@ -2,18 +2,18 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-	apiUrl: 'https://server.cristata.app/v3/troop-370',
-	plugins: {
-		'houdini-svelte': {
-			client: './src/client'
-		}
-	},
-	scalars: {
-		Date: { type: 'string' },
-		JSON: { type: 'string' },
-		ObjectID: { type: 'string' },
-		Void: { type: null }
-	}
+  apiUrl: 'https://server.cristata.app/v3/troop-370',
+  plugins: {
+    'houdini-svelte': {
+      client: './src/client',
+    },
+  },
+  scalars: {
+    Date: { type: 'string' },
+    JSON: { type: 'string' },
+    ObjectID: { type: 'string' },
+    Void: { type: null },
+  },
 };
 
 export default config;
