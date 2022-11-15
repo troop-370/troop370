eff97d4ae9d976c41266d48b28ffc2c05938bd25
Jack Buehner <jack.buehner@troop370atlanta.org>
2022-11-15 18:40:08 -0500

    fix: remove unused variable in fetchQuery

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { HoudiniClient, type RequestHandlerArgs } from '$houdini';
 
-async function fetchQuery({ fetch, text = '', variables = {}, metadata }: RequestHandlerArgs) {
+async function fetchQuery({ fetch, text = '', variables = {} }: RequestHandlerArgs) {
   const url = 'https://server.cristata.app/v3/troop-370';
   const result = await fetch(url, {
     method: 'POST',
