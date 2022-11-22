27a08e295778214ea2b2a7bd5849311edd59c599
Jack Buehner <jack.buehner@troop370atlanta.org>
2022-11-22 17:43:28 -0500

    feat: add outlined button component

---
+++ src/components/OutlinedButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Button, { Label } from '@smui/button';
+
+  export let href: string;
+</script>
+
+<Button variant="outlined" class="mdc-button-outlined--on-primary" {href}>
+  <Label><slot /></Label>
+</Button>
+
+<style lang="scss">
+  @use '@material/button/index' as mdc-button;
+  @use '@material/ripple/index' as mdc-ripple;
+
+  :global(.mdc-button-outlined--on-primary) {
+    @include mdc-button.ink-color(#e0e0e0);
+    @include mdc-button.outline-color(#e0e0e0);
+    margin: 15px 0;
+  }
+
+  :global(.mdc-button-outlined--on-primary .mdc-button__ripple) {
+    @include mdc-ripple.states-base-color('on-primary');
+  }
+
+  :global(.mdc-button-outlined--on-primary .mdc-button__label) {
+    text-transform: uppercase;
+    font-family: var(--font-headline);
+  }
+</style>
