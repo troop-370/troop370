d90d0bc9f0f5f8c98e36367cc3d2f8d7b105b93e
Jack Buehner <jack.buehner@troop370atlanta.org>
2022-11-22 20:10:40 -0500

    feat: wrap h2 and its children content in a div (only if not html tags are present inside h2 and its children)

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -1,5 +1,58 @@
 import { marked } from 'marked';
 
+const h2DivWrapExtension: marked.TokenizerExtension & marked.RendererExtension = {
+  name: 'h2DivWrap',
+  level: 'block',
+  start(src) {
+    return src.match(/:[^:\n]/)?.index; // Hint to Marked.js to stop and check for a match
+  },
+  tokenizer(src) {
+    const rule = /(?:^|\n)##\s[^\n]*\n(.*?)(?=\n##?\s|$)/gs;
+    const match = rule.exec(src);
+
+    if (match && !containsHTML(src)) {
+      const entireMatch = match[0];
+      const sectionContent = match[1];
+      const headingContent = entireMatch.replace(match[1], '');
+      const headingText = headingContent.replace('## ', '').replace('##', '').trim();
+
+      const headingToken: marked.Tokens.Heading = {
+        type: 'heading',
+        depth: 2,
+        raw: headingContent,
+        text: headingText,
+        tokens: this.lexer.inlineTokens(headingText),
+      };
+
+      const sectionTokens = this.lexer.blockTokens(sectionContent.trim(), []);
+
+      const token = {
+        type: 'h2DivWrap',
+        // We add the heading content to the entire match so that the length of raw
+        // matches the length of the generated text.
+        // Otherwise, the parser will assume that there is leftover text,
+        // which would result in a some text from the end of the provided
+        // markdown string being appended to the end of the generated HTML string
+        raw: headingContent + entireMatch,
+        text: entireMatch.trim(),
+        tokens: [headingToken, ...sectionTokens],
+      };
+
+      return token;
+    }
+  },
+  renderer(token) {
+    const maybeH2 = token.tokens?.[0];
+    if (isHeadingToken(maybeH2)) {
+      const slugger = new marked.Slugger();
+      const slug = 'section__' + slugger.slug(maybeH2.text);
+
+      return `<div id="${slug}">${this.parser.parse(token.tokens || [])}\n</div>`;
+    }
+
+    return `<div>${this.parser.parse(token.tokens || [])}\n</div>`;
+  },
+};
 class MarkDown {
   #renderer: marked.RendererObject = {
     heading(text, level) {
@@ -51,6 +104,7 @@ class MarkDown {
             return false;
           },
         },
+        h2DivWrapExtension,
       ],
     });
   }
@@ -127,10 +181,14 @@ class MarkDown {
 
 export const Markdown = new MarkDown();
 
-function isParagraphToken(toCheck: marked.Token): toCheck is marked.Tokens.Paragraph {
-  return toCheck.type === 'paragraph';
+function isParagraphToken(toCheck?: marked.Token): toCheck is marked.Tokens.Paragraph {
+  return toCheck?.type === 'paragraph';
 }
 
-function isHeadingToken(toCheck: marked.Token): toCheck is marked.Tokens.Heading {
-  return toCheck.type === 'heading';
+function isHeadingToken(toCheck?: marked.Token): toCheck is marked.Tokens.Heading {
+  return toCheck?.type === 'heading';
+}
+
+function containsHTML(toCheck?: string): boolean {
+  return !!toCheck?.match(/<\/?[a-z][\s\S]*>/);
 }
