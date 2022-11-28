f9d721e996c1a1e79d9b06e25608a505146efbf6
Jack Buehner <jack.buehner@troop370atlanta.org>
2022-11-28 16:57:14 -0500

    feat: use `sequence` in server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,12 @@
 import { COOKIE_SESSION_SECRET } from '$env/static/private';
+import { sequence } from '@sveltejs/kit/hooks';
 import { handleSession } from 'svelte-kit-cookie-session';
 
-export const handle = handleSession(
+const sessionHandler = handleSession(
   { secret: [{ id: 1, secret: COOKIE_SESSION_SECRET }] },
   ({ event, resolve }) => {
     return resolve(event);
   }
 );
+
+export const handle = sequence(sessionHandler);
