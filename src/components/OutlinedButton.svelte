87c567c324662ed93b552ceabc3a4152511b5e0d
Jack Buehner <jack.buehner@troop370atlanta.org>
2022-12-22 01:33:05 -0500

    fix: always apply mdc-button-outlined--on-primary styles

--- src/components/OutlinedButton.svelte
+++ src/components/OutlinedButton.svelte
@@ -7,17 +7,3 @@
 <Button variant="outlined" class="mdc-button-outlined--on-primary" {href}>
   <Label><slot /></Label>
 </Button>
-
-<style lang="scss">
-  @use '@material/button/index' as mdc-button;
-  @use '@material/ripple/index' as mdc-ripple;
-
-  :global(.mdc-button-outlined--on-primary) {
-    @include mdc-button.ink-color(#e0e0e0);
-    @include mdc-button.outline-color(#e0e0e0);
-  }
-
-  :global(.mdc-button-outlined--on-primary .mdc-button__ripple) {
-    @include mdc-ripple.states-base-color('on-primary');
-  }
-</style>
