9d7ab5bd6ac7d86b5130e6481a3205481163bd0a
Jack Buehner <jack.buehner@troop370atlanta.org>
2023-08-21 21:18:16 -0400

    fix: remove error

--- src/routes/(standard)/+layout.server.ts
+++ src/routes/(standard)/+layout.server.ts
@@ -3,17 +3,19 @@ import type { LayoutServerLoad } from './$houdini';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
   const { counter = 0, protectedPass = '' } = locals.session.data;
-  const authenticated = protectedPass === PROTECTED_PAGE_PASSWORD
+  const authenticated = protectedPass === PROTECTED_PAGE_PASSWORD;
 
   await locals.session.set({
     ...locals.session.data,
     counter: counter + 1,
     authenticated: authenticated,
-    authStrings: (authenticated ? {
-      password_message_when_authenticated: `Password: ${PROTECTED_PAGE_PASSWORD}`
-    } : {
-      password_message_when_authenticated: 'Password: ████████'
-    }) satisfies AuthStrings
+    authStrings: authenticated
+      ? {
+          password_message_when_authenticated: `Password: ${PROTECTED_PAGE_PASSWORD}`,
+        }
+      : {
+          password_message_when_authenticated: 'Password: ████████',
+        },
   });
 
   return {
