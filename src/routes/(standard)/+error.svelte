a1db0b1329d86b1fae9d67f554cd89c281a70d7e
Jack Buehner <jack.buehner@troop370atlanta.org>
2024-02-04 01:59:05 -0500

    feat: add error page

---
+++ src/routes/(standard)/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import Banner from '$components/Banner.svelte';
+</script>
+
+<Banner>
+  <h1>{$page.status} Error</h1>
+  <p>{$page.error.message}</p>
+</Banner>
