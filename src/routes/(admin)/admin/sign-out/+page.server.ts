67cdef9290bf94cb8d90d3c35850a3d8a546fa0f
Jack Buehner <jack.buehner@troop370atlanta.org>
2024-02-18 21:12:59 -0500

    feat: add admin sign out route

---
+++ src/routes/(admin)/admin/sign-out/+page.server.ts
@@ -0,0 +1,17 @@
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ fetch, locals }) => {
+  await fetch('/admin/strapi/admin/logout', {
+    headers: { Authorization: `Bearer ${locals.session.data.adminToken}` },
+  });
+
+  await locals.session.set({
+    ...locals.session.data,
+    adminEmail: undefined,
+    adminPass: undefined,
+    adminToken: undefined,
+    adminUser: undefined,
+  });
+
+  return {};
+}) satisfies PageServerLoad;
---
+++ src/routes/(admin)/admin/sign-out/+page.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const ssr = false;
+
+export const load = (async ({ fetch }) => {
+  localStorage.removeItem('jwtToken');
+  localStorage.removeItem('STRAPI_NPS_SURVEY_SETTINGS');
+  localStorage.removeItem('userInfo');
+  sessionStorage.removeItem('jwtToken');
+
+  throw redirect(302, '/admin/login');
+}) satisfies PageLoad;
