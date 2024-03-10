2e015d32b7f87908e26f950c5702584fd9593cd7
Jack Buehner <jack.buehner@troop370atlanta.org>
2024-03-10 01:29:13 -0500

    feat: get preview urls

--- src/routes/(admin)/admin/cms/+layout.ts
+++ src/routes/(admin)/admin/cms/+layout.ts
@@ -1,5 +1,45 @@
+import { queryWithStore } from '$utils/query';
+import { derived } from 'svelte/store';
+import { z } from 'zod';
 import type { LayoutLoad } from './$types';
 
-export const load = (async () => {
-  return {};
+export const load = (async ({ fetch, parent }) => {
+  const { session } = await parent();
+
+  const previewConfig = await queryWithStore<z.infer<typeof collectionPreviewConfigSchema>[]>({
+    fetch,
+    query: {
+      location: '/admin/strapi/preview-button/config',
+      opName: `strapiCollectionPreviewConfig`,
+      docsPath: 'config.contentTypes',
+      paginationPath: '',
+    },
+    validator: collectionPreviewConfigSchema.array(),
+    Authorization: `Bearer ${session.adminToken}`,
+    waitForQuery: true, // ensure that data is available before continuing since we need it in this function
+    useCache: true,
+    expireCache: 15 * 60 * 1000, // require refetch if it has been 15 minutes
+  }).then((store) => {
+    return derived([store], ([$store]) => {
+      return $store.data?.docs;
+    });
+  });
+
+  return { previewConfig };
 }) satisfies LayoutLoad;
+
+const collectionPreviewConfigSchema = z.object({
+  uid: z.string(),
+  published: z
+    .object({
+      url: z.string(),
+      query: z.record(z.string(), z.string()).optional(),
+    })
+    .optional(),
+  draft: z
+    .object({
+      url: z.string(),
+      query: z.record(z.string(), z.string()).optional(),
+    })
+    .optional(),
+});
