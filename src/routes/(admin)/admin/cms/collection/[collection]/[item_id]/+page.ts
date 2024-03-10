9379c8fc4e5eaa3366dc17cb18889557f069ed4d
Jack Buehner <jack.buehner@troop370atlanta.org>
2024-03-10 01:32:29 -0500

    feat: get document data with relations

---
+++ src/routes/(admin)/admin/cms/collection/[collection]/[item_id]/+page.ts
@@ -0,0 +1,45 @@
+import { query } from '$utils/query';
+import { z } from 'zod';
+import type { PageLoad } from './$types';
+
+export const load = (async ({ fetch, params, parent, url }) => {
+  const { session, settings } = await parent();
+
+  const docData = await query<any>({
+    fetch,
+    query: {
+      location: '/admin/strapi/content-manager/collection-types/api::post.post/' + params.item_id,
+      opName: `collectionItemData__${params.collection}__${params.item_id}`,
+      docsPath: '',
+    },
+    Authorization: `Bearer ${session.adminToken}`,
+    validator: z.any(),
+  }).then((res) => res?.data?.docs);
+
+  await Promise.all(
+    settings.defs
+      .filter(([, def]) => def.type === 'relation')
+      .map(async ([key, def]) => {
+        const relationData = await query<any>({
+          fetch,
+          query: {
+            location: `/admin/strapi/content-manager/relations/api::post.post/${params.item_id}/${key}`,
+            opName: `collectionItemData__${params.collection}__${params.item_id}`,
+            docsPath: def.relationType === 'oneToOne' ? 'data' : 'results',
+          },
+          variables: {
+            pageSize: 100,
+          },
+          Authorization: `Bearer ${session.adminToken}`,
+          validator: z.any(),
+        }).then((res) => res?.data?.docs);
+
+        docData[key] = relationData;
+      })
+  );
+
+  return {
+    url,
+    docData,
+  };
+}) satisfies PageLoad;
