da16cde0915f6be5bfbddece9ca2975a09253d10
Jack Buehner <jack.buehner@troop370atlanta.org>
2024-09-22 17:12:37 -0400

    fix: do not put strapi search query in filters

--- src/routes/(admin)/poptart/cms/collection/[uid]/+page.ts
+++ src/routes/(admin)/poptart/cms/collection/[uid]/+page.ts
@@ -153,7 +153,7 @@ function paramsToFilter(search: URLSearchParams) {
   const result: Record<string, string> = {};
   for (const [key, value] of search.entries()) {
     // each 'entry' is a [key, value] tuple
-    if (!key.startsWith('__')) {
+    if (!key.startsWith('__') && key !== '_search') {
       if (value.startsWith('"') && value.endsWith('"')) result[key] = value.slice(1, -1);
       else if (isJSON(value)) result[key] = JSON.parse(value);
       else result[key] = value;
