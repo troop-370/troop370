dfd7aca271fdea3dde907206a5fe524b29005b29
Jack Buehner <jack.buehner@troop370atlanta.org>
2024-11-22 19:49:50 -0500

    fix(admin, editor): wait for versions store to finish loading before selecting the data for the requested version

--- src/routes/(admin)/poptart/cms/collection/[uid]/[documentId]/version/[version_id]/+page.ts
+++ src/routes/(admin)/poptart/cms/collection/[uid]/[documentId]/version/[version_id]/+page.ts
@@ -8,6 +8,13 @@ export const load = (async ({ parent, params, fetch }) => {
   const { session, versions, collectionConfig, permissions } = await parent();
 
   const versionId = params.version_id;
+
+  // if the store is loading, wait for it to finish
+  while (get(versions)?.loading) {
+    // wait for the store to update
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+
   const version = get(versions)?.data?.data?.find((v) => v.id === Number(versionId));
 
   const defs = filterSchemaDefs(get(collectionConfig).defs, permissions, ['read']);
