3922fe0caece61242226fbd98f5b8383c156f5cb
Jack Buehner <jack.buehner@troop370atlanta.org>
2025-01-01 17:57:52 -0500

    feat(admin): add more data to developer dialog

--- src/routes/(admin)/poptart/cms/collection/[uid]/[documentId]/DeveloperDialog.svelte
+++ src/routes/(admin)/poptart/cms/collection/[uid]/[documentId]/DeveloperDialog.svelte
@@ -4,6 +4,11 @@
 
   export let data: PageData;
   export let open = false;
+
+  $: ({ permissions, contentTypeSchema, collectionConfig, session, ...rest } = data);
+  $: ({ attributes, layouts, metadatas, ...settings } = $collectionConfig as NonNullable<
+    typeof $collectionConfig
+  >);
 </script>
 
 <ContentDialog bind:open title="Developer" size="max" class="developer-dialog">
@@ -11,6 +16,43 @@
     Document data
     <pre slot="content">{JSON.stringify(data.docData, null, 2)}</pre>
   </Expander>
+
+  {#if $contentTypeSchema && Object.keys(contentTypeSchema).length > 0}
+    <Expander>
+      Schema
+      <svelte:fragment slot="content">
+        <pre>{JSON.stringify($contentTypeSchema, null, 2)}</pre>
+      </svelte:fragment>
+    </Expander>
+  {/if}
+
+  {#if settings && Object.keys(settings).length > 0}
+    <Expander>
+      Collection settings
+      <svelte:fragment slot="content">
+        <pre>{JSON.stringify(settings, null, 2)}</pre>
+      </svelte:fragment>
+    </Expander>
+  {/if}
+
+  {#if session && Object.keys(session).length > 0}
+    <Expander>
+      Session
+      <svelte:fragment slot="content">
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </svelte:fragment>
+    </Expander>
+  {/if}
+
+  {#if rest && Object.keys(rest).length > 0}
+    <Expander>
+      Rest
+      <svelte:fragment slot="content">
+        <pre>{JSON.stringify(rest, null, 2)}</pre>
+      </svelte:fragment>
+    </Expander>
+  {/if}
+
   <Button slot="footer" on:click={() => (open = false)}>Close</Button>
 </ContentDialog>
 
