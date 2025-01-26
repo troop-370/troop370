eabbf11d7a98bbfb93f36f506e74f28bbaaf80cf
Jack Buehner <jack.buehner@troop370atlanta.org>
2025-01-26 18:02:03 -0500

    feat: accept admin panel authentication in place of member password authentication

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,20 @@
 import { PROTECTED_PAGE_PASSWORD } from '$env/static/private';
+import { jwtDecode } from 'jwt-decode';
 import type { LayoutServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
-  // handle authentication
-  const { counter = 0, protectedPass = '' } = locals.session.data;
-  const authenticated = protectedPass === PROTECTED_PAGE_PASSWORD;
+  const { counter = 0, protectedPass = '', adminToken } = locals.session.data;
+
+  // if the user has entered the correct password, then they are authenticated
+  let authenticated = protectedPass === PROTECTED_PAGE_PASSWORD;
+
+  // alternatively, if the admin panel token is present and not expired, the user is authenticated
+  if (!authenticated && adminToken) {
+    const tokenExpiresAt = new Date((jwtDecode(adminToken).exp || 0) * 1000);
+    const tokenIsExpired = tokenExpiresAt < new Date();
+
+    authenticated = !tokenIsExpired;
+  }
 
   await locals.session.set({
     ...locals.session.data,
