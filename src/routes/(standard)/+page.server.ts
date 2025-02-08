d7945f2d890fbdfa302071a5a4548b8904976590
Jack Buehner <github@jackbuehner.com>
2025-02-08 00:48:25 -0500

    feat: add spring 2025 pine straw sale announcement

--- src/routes/(standard)/+page.server.ts
+++ src/routes/(standard)/+page.server.ts
@@ -1,32 +1,37 @@
 import { apity } from '$api';
+import { notEmpty } from '$utils';
 import type { PageServerLoad } from './$types';
 
-const getHomePageConfig = apity.path('/home-page').method('get').create();
+const getHomePageConfig = apity.path("/home-page").method("get").create();
 
 export const load: PageServerLoad = async ({ fetch }) => {
   const { result } = getHomePageConfig(
     {
-      populate: 'announcement_cards, announcement_cards.background_photo',
+      populate: "announcement_cards, announcement_cards.background_photo",
     },
     fetch
   );
   const resolved = await result;
 
-  const announcementCards = resolved.ok ? resolved?.data?.data?.announcement_cards || [] : [];
+  const announcementCards = resolved.ok
+    ? resolved?.data?.data?.announcement_cards || []
+    : [];
 
   return {
     announcementCards: [
-      {
-        id: '1',
-        title: 'Pine Straw Sale',
-        subtitle: 'Order by October 26',
-        link: '/pay/pinestraw',
-        link_text: 'Order now',
-        background_photo: {
-          url: 'https://troop370atlanta.org/photos/backgrounds/pineneedles_l.jpg',
-        },
-      },
+      new Date() <= new Date("2025-03-15")
+        ? {
+            id: "1",
+            title: "Pine Straw Sale",
+            subtitle: "Order by March 15",
+            link: "/pay/pinestraw",
+            link_text: "Order now",
+            background_photo: {
+              url: "https://troop370atlanta.org/photos/backgrounds/pineneedles_l.jpg",
+            },
+          }
+        : null,
       ...announcementCards,
-    ],
+    ].filter(notEmpty),
   };
 };
